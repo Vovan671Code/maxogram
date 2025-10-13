80144fd7629b4a299775894637ba12bc70f54f62
Vladimir Nesterenko <vladimirnesterenko448@gmail.com>
2025-10-13 18:38:02 +0300

    Initial commit, base account features added

---
+++ social_media/account/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', max_length=100, widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username','first_name','email',)
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name','last_name','email',)
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
---
+++ social_media/account/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+from .views import dashboard
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login'),
+    #
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #
+    # path('password-reset/',auth_views.PasswordResetView.as_view(html_email_template_name="registration/password_reset_email.html"),name='password_reset'),
+    # path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    # path('password-reset/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
+
