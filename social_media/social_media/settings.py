48ab89c3939a1b2810980effaa2ffa67a8e304db
Vladimir Nesterenko <vladimirnesterenko448@gmail.com>
2025-11-04 15:11:45 +0300

    Bookmark create page added

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "social_django",
     "django_extensions",
+    "images.apps.ImagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -161,3 +162,16 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env("GOOGLE_OAUTH2_KEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env("GOOGLE_OAUTH2_SECRET")
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
